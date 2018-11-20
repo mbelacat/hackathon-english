93e1b2f0dd43c98a0687bd42f19b192cf1937acc
Sebastien <sebastien.munoz@outlook.fr>
2018-11-20 15:41:59 +0100

    Delete css no exist

---
+++ Template/Forms/formCreateQuestion.php
@@ -0,0 +1,17 @@
+<?php include "../header.php"; ?>
+<form>
+    <div class="form-group">
+        <label for="question">Question</label>
+        <input type="text" class="form-control" id="question" placeholder="Question">
+  </div>
+  
+  <div class="form-check form-check-inline">
+    <input class="form-check-input" type="checkbox" id="inlineCheckbox1" value="option1">
+    <label class="form-check-label" for="inlineCheckbox1">1</label>
+    </div>
+    <div class="form-check form-check-inline">
+        <input class="form-check-input" type="checkbox" id="inlineCheckbox2" value="option2">
+        <label class="form-check-label" for="inlineCheckbox2">2</label>
+    </div>
+</form>
+<?php include "../footer.php"; ?>
