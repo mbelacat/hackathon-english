edb611363a157589f73c4052e7222d8cee2f27c8
Mbela Mbemba <34045621+mbelacat@users.noreply.github.com>
2018-11-21 19:25:48 +0100

    Update homeSecretary.php
    change form name formCreatedSessionStudent.php to formCreatedSession.php

--- admin/homeSecretary.php
+++ admin/homeSecretary.php
@@ -2,7 +2,7 @@
 include "../Template/header.php";
  ?>
 
-<a href="../Template/Forms/formCreatedSessionStudent.php" class="btn btn-primary btn-lg active" role="button" aria-pressed="true">Inscription</a>
+<a href="../Template/Forms/formCreatedSession.php" class="btn btn-primary btn-lg active" role="button" aria-pressed="true">Inscription</a>
 <a href="../admittedList.php" class="btn btn-primary btn-lg active" role="button" aria-pressed="true">Résultat</a>
 <a href="../sessionList.php" class="btn btn-primary btn-lg active" role="button" aria-pressed="true">Table de progression</a>
 
