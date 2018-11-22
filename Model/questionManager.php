522f3e1d46dfc6c457b56072639db0c5e0f0d592
Nassim <nassim19961996@outlook.fr>
2018-11-22 14:53:47 +0100

    Add Question/Reponse systeme

--- Model/questionManager.php
+++ Model/questionManager.php
@@ -1,1 +1,17 @@
+<?php
 
+//Fonction qui récupère le dernière ID de la question
+function getLastQuestionID($db){
+
+
+$selectLast = $db->query('SELECT id_question FROM question ORDER BY id_question DESC LIMIT 0, 1');
+
+$lastId = $selectLast->fetch(PDO::FETCH_ASSOC);
+
+return $lastId['id_question']; // $lastId['id'] cette variable contient le dernier id<br>
+
+$selectLast->closeCursor();
+
+}
+
+?>
