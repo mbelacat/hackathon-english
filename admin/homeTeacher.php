d51ce824b176ca782fc760d7fea5078e144c2e61
Gino <ginovito@live.fr>
2018-11-22 16:55:43 +0100

    add updateQuestionTreatment.php

--- admin/homeTeacher.php
+++ admin/homeTeacher.php
@@ -2,7 +2,7 @@
 
   <p class="text-right"><a type="button" href="logout.php" class="btn btn-success text-white ">Déconnecter</a></p>
 <section class="text-center d-flex justify-content-around m-5">
-  <span class="circle"><a href="../questionsList.php" type="button" class="btn btn-secondary">QCM</a></span>
+  <span class="circle"><a href="../questionList.php" type="button" class="btn btn-secondary">QCM</a></span>
   <span class="circle"><a href="../admittedList.php" type="button" class="btn btn-secondary">Apprenant</a></span>
 </section>
 
