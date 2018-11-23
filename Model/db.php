a8020d36665a575dbecc2542d15d47def7a4a6bf
Sebastien <sebastien.munoz@outlook.fr>
2018-11-23 14:37:55 +0100

    Update structure and correct divers bugs

--- Model/db.php
+++ Model/db.php
@@ -3,7 +3,7 @@ function connectToDataBAse()
 {
   try
   {
-    $db = new PDO('mysql:host=localhost;dbname=Adep_Anglais;charset=utf8', 'root', 'root');
+    $db = new PDO('mysql:host=localhost;dbname=Adep_Anglais;charset=utf8', 'phpmyadmin', 'root');
   }
   catch (Exception $e)
   {
--- questionList.php
+++ questionList.php
@@ -1,7 +1,6 @@
 <?php
 require "Service/errorManager.php";
 include "Template/header.php";
-require "Service/errorManager.php";
 displayMessages();
 ?>
 <!-- Liste des questions que le prof a crée, visible depuis son espace admin -->
