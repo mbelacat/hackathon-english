d3a21b8a006f36e6c5deeafad5e9f292a86864c6
Sebastien <sebastien.munoz@outlook.fr>
2018-11-23 10:06:54 +0100

    Update structure and correct divers bugs

--- admin/homeSecretary.php
+++ admin/homeSecretary.php
@@ -1,5 +1,6 @@
 <?php include "../Template/header.php"; ?>
 
+<p class="text-right"><a type="button" href="../logout.php" class="btn btn-success text-white ">Déconnecter</a></p>
 <a href="createSessionStudent.php" class="btn btn-primary btn-lg active" role="button" aria-pressed="true">Inscription</a>
 <a href="../admittedList.php" class="btn btn-primary btn-lg active" role="button" aria-pressed="true">Résultat</a>
 <a href="../sessionList.php" class="btn btn-primary btn-lg active" role="button" aria-pressed="true">Table de progression</a>
