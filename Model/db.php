caba5ef56eff4dbd46194968dca5738af951cc9a
Simon <julietteroffe@MacBook-Pro-de-Juliette.local>
2018-11-23 10:01:25 +0100

    le ptit pull du matin

--- Model/db.php
+++ Model/db.php
@@ -3,7 +3,7 @@ function connectToDataBAse()
 {
   try
   {
-    $db = new PDO('mysql:host=localhost;dbname=Adep_Anglais;charset=utf8', 'phpmyadmin', 'password');
+    $db = new PDO('mysql:host=localhost;dbname=Adep_Anglais;charset=utf8', 'root', 'root');
   }
   catch (Exception $e)
   {
--- sessionList.php
+++ sessionList.php
@@ -20,7 +20,7 @@ include "Template/header.php";
       <th scope="col">Supprimer</th>
     </tr>
   </thead>
-  
+
   <tbody>
   <!-- foreach to see all sessions -->
   <?php
@@ -30,7 +30,7 @@ include "Template/header.php";
     <tr>
       <th scope="row"> <?php echo $result["last_name"] ?> </th>
       <td scope="row"> <?php echo $result["first_name"] ?> </td>
-<<<<<<< HEAD
+
 <?php
     }
 
@@ -43,20 +43,9 @@ foreach ($code as $key => $theKey) {
     <td scope="row"><a href="<?php echo 'deleteSessionTreatment.php?id=' . $theKey['id_session']; ?>" class="btn btn-danger">Supprimer</a></td>
 </tr>
 <?php
-=======
-      <?php
->>>>>>> f246d3355a6cd47c11b2f8e048633d13b127df3d
-  }
-    foreach ($code as $key => $theKey)
-    {
-    ?>
-      <td scope="row">  <?php echo $theKey["code"] ?> </td>
-      <td scope="row"><?php echo $theKey["created_date"] ;  ?> </td>
-      <td scope="row"><i class="fas fa-trash-alt"></i></td>
-      </tr>
-      <?php
-    }
-    ?>
+}
+?>
+
   </tbody>
 </table>
 <?php include "Template/footer.php"; ?>
