72ceb43b729ee3b2d5e3e838d0aa1d203e93f3b0
mbela mbemba <mbela.mbemba@gmail.com>
2018-11-23 12:09:21 +0100

    fixed bug

--- Model/db.php
+++ Model/db.php
@@ -3,13 +3,7 @@ function connectToDataBAse()
 {
   try
   {
-<<<<<<< HEAD
     $db = new PDO('mysql:host=localhost;dbname=Adep_Anglais;charset=utf8', 'mbela', 'rootroot');
-=======
-    
-    $db = new PDO('mysql:host=localhost;dbname=Adep_Anglais;charset=utf8', 'root', 'root');
-
->>>>>>> 8324a2ca7b38ac27ddad17a8fd2e1c9f9cbed054
   }
   catch (Exception $e)
   {
